11d32b4bd8161bdc9658cc46106ddaeea690900f
bhavanawagh <bhavanawagh3011@gmail.com>
2020-05-29 16:39:57 +0530

    In this app.js file, first of all i have created a class of Book Which Represents a Book and in which i have taken constructor of title, author and isbn. After that there is a UI Class which handles the all UI tasks, in this UI class i have taken static method of displayBooks which stores the books, after that there is another static method of addBookToList, by which i can add book with a perticular formate with hardcode. After this, created a addEventListener of DOMcontentLoaded which calls the method of displayBooks of UI class. After this another AddEventListener of submit button by which i can submit title author and isbn.

---
+++ app.js
@@ -0,0 +1,63 @@
+// Book Class: Represents a Book
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
+}
+
+// UI Class: Handle UI Tasks
+class UI {
+  static displayBooks() {
+    const StoreBooks = [
+      {
+        title: 'Book One',
+        author: 'Bhavana Wagh',
+        isbn: '3454454'
+      },
+      {
+        title: 'Book Two',
+        author: 'Atul Chavan',
+        isbn: '3454454'
+      }
+    ];
+
+    const books = StoreBooks;
+    books.forEach((book) => UI.addBookToList(book));
+  }
+
+  static addBookToList(book){
+    const list = document.querySelector('#book-list');
+    const row = document.createElement('tr');
+    row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+    `;
+    list.appendChild(row);
+  }
+}
+
+// Event: Display Books
+  document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+// Event: Add a Book
+  document.querySelector('#book-form').addEventListener('submit', 
+(e) => {
+//Prevent acutal submit
+e.preventDefault();
+
+// Get form values
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const isbn = document.querySelector('#isbn').value;
+
+// Instatiate book
+const book = new Book(title, author, isbn);
+
+//Add Book to UI
+ UI.addBookToList(book); 
+});
+
